dea582605853a5347fbdf8f342211395cb02e3f2
WTFox <Anthonyfox1988@Gmail.com>
2017-05-15 19:40:54 -0500

    Style button on main page

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -14,7 +14,7 @@ var WeatherForm = React.createClass({
       <div>
         <form onSubmit={this.onFormSubmit}>
           <input ref="location" type="text"/>
-          <button>Get Weather</button>
+          <button className="button expanded hollow">Get Weather</button>
         </form>
       </div>
     )
