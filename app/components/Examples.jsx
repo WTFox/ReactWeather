cf521b28235cecdf477149edffe088c9a953e406
WTFox <Anthonyfox1988@Gmail.com>
2017-05-15 19:27:51 -0500

    Style Examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // for components that do no require maintaining state,
 // you can use arrow functions. Available in React > 14?
 var Examples = (props) => {
     return (
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to the examples page</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+          <li>
+            <Link to="/?location=Nashville">Nashville, TN</Link>
+          </li>
+          <li>
+            <Link to="/?location=Orange%20County">Orange County, CA</Link>
+          </li>
+        </ol>
       </div>
     )
 };
